7dace767b3c8f4d4eeeb23b84203387fce9c6d2c
Geannina <geannina@zarmada.com>
2019-02-22 08:27:57 +1300

    Updating lab implementation

--- BuildAnIntelligentBot/src/ChatBot/EchoBotAccessors.cs
+++ BuildAnIntelligentBot/src/ChatBot/EchoBotAccessors.cs
@@ -9,7 +9,7 @@ namespace ChatBot
 {
     /// <summary>
     /// This class is created as a Singleton and passed into the IBot-derived constructor.
-    ///  - See <see cref="EchoWithCounterBot"/> constructor for how that is injected.
+    ///  - See <see cref="EchoBot"/> constructor for how that is injected.
     ///  - See the Startup.cs file for more details on creating the Singleton that gets
     ///    injected into the constructor.
     /// </summary>
