51b5b82e32a4365c6192cb10a888cc351e2e5c1a
Daniel Cáceres <daniel.caceresm@outlook.com>
2020-05-19 09:00:38 +0200

    XamlIsland projects structure changed to avoid XamlParseException.

--- VirtualStage/Speaker.Recorder/Speaker.Recorder.UWP/App.xaml.cs
+++ VirtualStage/Speaker.Recorder/Speaker.Recorder.UWP/App.xaml.cs
@@ -8,5 +8,11 @@ namespace Speaker.Recorder.UWP
         {
             this.InitializeComponent();
         }
+
+        public void Include()
+        {
+            var t1 = typeof(Microsoft.Xaml.Interactivity.Behavior);
+            var t2 = typeof(Microsoft.Xaml.Interactions.Core.CallMethodAction);
+        }
     }
 }
